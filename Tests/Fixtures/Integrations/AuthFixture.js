46af9d1a91b77c8a0885c44bc82a3c9f84a18b6e
Mahan <mahan@gomeeki.com>
2017-07-30 12:38:17 +0700

    fix

--- Tests/Fixtures/Integrations/AuthFixture.js
+++ Tests/Fixtures/Integrations/AuthFixture.js
@@ -78,7 +78,7 @@ export default class AuthFixture {
 
   static async cleanUp () {
     await Model.Users.update({ avatarResourceId: null }, { where: {} })
-    
+
     await Model.UsersHashtags.destroy({ where: {} })
     await Model.VideosThumbnails.destroy({ where: {} })
     await Model.VideosHashtags.destroy({ where: {} })
