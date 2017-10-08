647de8404d6747cdb8bd10e65e4861a12a59c744
Mahan <mahan@gomeeki.com>
2017-10-08 18:39:50 +0700

    add user to message data

--- App/Repositories/MessagesRepository.js
+++ App/Repositories/MessagesRepository.js
@@ -30,23 +30,65 @@ export default class MessagesRepository {
 
     const ProducedMap = {
       'message_id': 'id',
-      'message_sender_user_id': 'senderUserId',
-      'message_receiver_user_id': 'receiverUserId',
       'message_message': 'message',
       'message_type': 'type',
       'message_sent_at': 'sendAt',
       'message_seen_at': 'seenAt',
+      'message_sender_user': 'senderUser',
+      'message_receiver_user': 'receiverUser',
     }
 
     const users = await Models.sequelize.query(`
       SELECT
         m.id                AS message_id,
-        m.sender_user_id    AS message_sender_user_id,
-        m.receiver_user_id  AS message_receiver_user_id,
         m.message           AS message_message,
         m.type              AS message_type,
         m.sent_at           AS message_sent_at,
-        m.seen_at           AS message_seen_at
+        m.seen_at           AS message_seen_at,
+        (
+        
+          SELECT array_to_json(array_agg(isu)) AS hashtags
+          FROM
+           (
+              SELECT
+                su.id           AS id,
+                su.is_activated AS isActivated,
+                su.gender       AS gender,
+                su.email        AS email,
+                su.first_name   AS firstName,
+                su.last_name    AS lastName,
+                su.username     AS username,
+                r.cloud_url     AS cloudAvatarUrl
+              FROM users AS su
+                LEFT JOIN resources AS r
+                  ON r.id = su.avatar_resource_id 
+              WHERE
+                su.id = m.sender_user_id
+           ) AS isu
+
+        )                   AS message_sender_user,
+        (
+        
+          SELECT array_to_json(array_agg(isu)) AS hashtags
+          FROM
+           (
+              SELECT
+                su.id           AS id,
+                su.is_activated AS isActivated,
+                su.gender       AS gender,
+                su.email        AS email,
+                su.first_name   AS firstName,
+                su.last_name    AS lastName,
+                su.username     AS username,
+                r.cloud_url     AS cloudAvatarUrl
+              FROM users AS su
+                LEFT JOIN resources AS r
+                  ON r.id = su.avatar_resource_id 
+              WHERE
+                su.id = m.receiver_user_id
+           ) AS isu
+                      
+        )                   AS message_receiver_user
       FROM messages AS m
       WHERE
         (
